7559dd8719cb87eca70011e49897b841f90f034e
Tom Myruski <thomas.myruski@gmail.com>
2018-01-23 17:36:14 -0500

    Added soc icons to header

--- template-parts/header-menu.php
+++ template-parts/header-menu.php
@@ -1,4 +1,8 @@
- <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+    <div class="soc-menu-icons">
+      <a href="https://www.facebook.com/The-Press-House-215959480570/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+      <a href="https://twitter.com/sarahjane_frost" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+      <a href="https://www.instagram.com/thepresshousepr/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+</div><div class="collapse navbar-collapse navbar-right navbar-main-collapse">
       <ul class="nav navbar-nav">
         <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
       
@@ -9,6 +13,11 @@
         <li> <a class="page-scroll" href="<?php echo get_site_url() ; ?>/tour">Tour</a> </li>
         <li> <a class="page-scroll" href="<?php echo get_site_url() ; ?>/press-release">Releases</a> </li>
          <li> <a class="page-scroll" href="<?php echo get_site_url() ; ?>/services">Services</a> </li>
+             <div class="soc-menu-icons-mobile">
+      <a href="https://www.facebook.com/The-Press-House-215959480570/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+      <a href="https://twitter.com/sarahjane_frost" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+      <a href="https://www.instagram.com/thepresshousepr/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+</div>
        </li><li>
        <form role="search" method="get" id="searchform" class="searchform navbar-form" action="<?php echo get_site_url() ; ?>"> 
 <!-- <label class="screen-reader-text" for="s">Search for:</label>  -->
